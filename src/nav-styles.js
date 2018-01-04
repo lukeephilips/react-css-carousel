be6a0690455e60aceaab58ca2c61aa91fc5f58af
Luke <luke.e.philips@gmail.com>
2018-01-04 12:14:09 -0800

    rebuild with radium

--- src/nav-styles.js
+++ src/nav-styles.js
@@ -1,5 +1,17 @@
+import Radium from 'radium';
 import config from './config-styles';
 
+const pulse = Radium.keyframes({
+  '0%': {
+    transform: 'scale3d(1, 1, 1)',
+  },
+  '15%': {
+    transform: 'scale3d(1.05, 1.05, 1.05)',
+  },
+  '100%': {
+    transform: 'scale3d(1, 1, 1)',
+  },
+}, 'Nav')
 const btn = {
   flex: 1,
   height: config.imageHeight,
@@ -12,6 +24,11 @@ const btn = {
   outline: 0,
   cursor: 'pointer',
   userSelect: 'none',
+  animation: `${pulse} 4s 2s infinite`,
+  ':hover': {
+    transition: 'all 1s',
+    color: '#8c9ea3',
+  },
 }
 
 const prev = {
