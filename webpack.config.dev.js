8bbc526d9f77ea874b327074764d4c0afd05e8e2
Luke <luke.e.philips@gmail.com>
2018-01-04 21:28:03 -0800

    add react-styleable

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,8 +22,11 @@ module.exports = {
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
     }, {
-      test: /\.jpg/, 
+      test: /\.jpg/,
       loader: 'file'
+    }, {
+      test: /\.css/,
+      loaders: ['style', 'css?modules&localIdentName=[local]---[hash:base64:5]', 'cssnext']
     }]
   }
 }
